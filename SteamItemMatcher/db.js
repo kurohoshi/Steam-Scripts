503d4e43488905f6d9cedf2c9dbb8609108c2ee2
Lingxing Zheng <xingling39@gmail.com>
2024-03-16 16:16:23 -0500

    Some refactoring and feature changes
     - Modified setup method to dynamically generate objectStores and indexes based on a configuration list. This will need to be modified in the future for upgrading db versions rather than creating from scratch.
     - Changed some logical ORs to the more appropriate nullish coalescing operator
     - Added the ability to get profiles based on custom URL strings
     - Modified some method parameters to accept a higher amount of specific variables for better flexibility... hopefully
     - Removal of some whitespaces

--- SteamItemMatcher/db.js
+++ SteamItemMatcher/db.js
@@ -12,21 +12,34 @@ const SteamToolsDbManager = {
          dbReq.onblocked = event => {
             alert("New Steam Tools database version detected, please close all tabs of this site!");
          }
-         
+
          dbReq.onupgradeneeded = event => {
             this.db = event.target.result;
 
             if(event.oldVersion === 0) {
-               // NOTE: not keys/indices, we create our own key on add(), then create our own keyrange later
-               this.db.createObjectStore("profiles");
-               this.db.createObjectStore("badgepages");
-               this.db.createObjectStore("app_data");
-               this.db.createObjectStore("item_descripts");
-               this.db.createObjectStore("inventories");
-               this.db.createObjectStore("item_matcher_results");
+               // NTOE: objconfig should be validated
+               for(let objConfig of DB_OBJECTSTORE_CONFIGS) {
+                  if(!objConfig.keypath && !objConfig.autoincr) {
+                     this.db.createObjectStore(objConfig.name);
+                  } else {
+                     let options = {};
+                     if(typeof objConfig.keypath === 'string') {
+                        options.keyPath = objConfig.keypath;
+                     }
+                     if(objConfig.autoincr) {
+                        options.autoincr = true;
+                     }
+                     let newObjStore = this.db.createObjectStore(objConfig.name, options);
+                     if(objConfig.indices && Array.isArray(objConfig.indices)) {
+                        for(let indexerEntry of objConfig.indices) {
+                           newObjStore.createIndex(indexerEntry.name, indexerEntry.keyPath, indexerEntry.options);
+                        }
+                     }
+                  }
+               }
             }
          }
-         
+
          dbReq.onsuccess = event => {
             this.db = event.target.result;
             this.db.onversionchange = event => {
@@ -36,7 +49,7 @@ const SteamToolsDbManager = {
             console.log("Database is set up!");
             resolve();
          }
-         
+
          dbReq.onerror = event => {
             reject("Error opening database. Error code: " + event.target.errorCode);
          }
@@ -51,9 +64,9 @@ const SteamToolsDbManager = {
       return true;
    },
    // get multiple: probably used indexrange+getAll, or iteratively execute get with the same or batches of transactions
-   get: function(ObjStoreName, keys, successCb) {
+   get: function(ObjStoreName, indexName, keys, successCb) {
       const MAX_REQ = 10;
-      if(typeof keys === "string") {
+      if(!Array.isArray(keys)) {
          keys = [keys];
       }
 
@@ -64,30 +77,38 @@ const SteamToolsDbManager = {
          }
 
          let result = {};
+
          let transactions = Array(Math.ceil(keys.length/MAX_REQ));
-         for(let i=0; i<transactions.length; ) {
+         for(let i=0; i<transactions.length; i++) {
             transactions[i] = new Promise((res, rej) => {
+
                let trans = this.db.transaction([ObjStoreName], "readonly");
                trans.oncomplete = event => {
                   res();
                }
-               
-               for(let j=i*MAX_REQ, lim=(i+1)*MAX_REQ; j<keys.length && j<lim; j++) {
-                  let objStoreReq = trans.objectStore(ObjStoreName).get(keys[j]);
-                  objStoreReq.onsuccess = event => {
+               const getValue = (transaction, startIndex, offset) => {
+                  let objStoreReq = transaction.objectStore(ObjStoreName)
+                  if(indexName) {
+                     objStoreReq = objStoreReq.index(indexName);
+                  }
+                  objStoreReq.get(keys[startIndex+offset]);
+                  objStoreReq.onsuccess = (event) => {
                      let cbResult;
                      if(typeof successCb === 'function') {
                         cbResult = successCb(event);
                      }
-                     if(cbResult === undefined) {
-                        cbResult = event.target.result;
+                     cbResult ??= event.target.result;
+                     result[keys[startIndex+offset]] = cbResult;
+
+                     if((offset+1 < MAX_REQ) && (startIndex+offset+1<keys.length)) {
+                        getValue(event.target.transaction, startIndex, offset+1);
                      }
-                     result[keys[j]] = cbResult;
                   }
-                  objStoreReq.onerror = event => {
+                  objStoreReq.onerror = (event) => {
                      rej();
                   }
-               }
+               };
+               getValue(trans, i*MAX_REQ, 0);
             });
          }
          Promise.all(transactions).then(() => resolve(result));
@@ -117,67 +138,71 @@ const SteamToolsDbManager = {
          }
       });
    },
-   getProfiles: async function(profileids) {
-      return await this.get("profiles", profileids);
+   getProfiles: async function(profileids, useURL=false) {
+      return useURL
+         ? (await this.get("profiles", 'url', profileids))
+         : (await this.get("profiles", undefined, profileids));
    },
    setProfile: async function(profile) {
       let savedData = await this.get("profiles", profile.id);
       savedData = savedData[profile.id] ? savedData[profile.id] : {};
-      savedData.id         = profile.id || savedData.id;
-      savedData.url        = profile.url || savedData.url;
-      savedData.name       = profile.name || savedData.name;
-      savedData.pfp        = profile.pfp || savedData.pfp;
-      savedData.state      = profile.state || savedData.state;
-      savedData.tradeToken = profile.tradeToken || savedData.tradeToken;
-      savedData.friends    = profile.friends || savedData.friends;
+      savedData.id         ??= profile.id;
+      savedData.url        ??= profile.url;
+      savedData.name       ??= profile.name;
+      savedData.pfp        ??= profile.pfp;
+      savedData.state      ??= profile.state;
+      savedData.tradeToken ??= profile.tradeToken;
+      savedData.friends    ??= profile.friends;
+      savedData.last_updated ??= profile.last_updated;
 
       await this.set("profiles", savedData, profile.id);
    },
    getBadgepages: async function(profileids) {
       return await this.get("badgepages", profileids);
    },
-   setBadgepages: async function(profile) {
-      let savedData = await this.get("badgepages", profile.id);
-      if(savedData[profile.id]) {
-         savedData = savedData[profile.id];
-         for(let [rarity, appList] of profile.badgepages.entries()) {
+   setBadgepages: async function(profileid, badgepages) {
+      let savedData = await this.get("badgepages", profileid);
+      if(savedData[profileid]) {
+         savedData = savedData[profileid];
+         for(let [rarity, appList] of badgepages.entries()) {
             for(let [appid, data] of Object.entries(appList)) {
-               savedData[rarity][appid] = data;
+               if(data.last_updated>savedData[rarity][appid].last_updated) {
+                  savedData[rarity][appid] = data;
+               }
             }
          }
       } else {
-         savedData = profile.badgepages;
+         savedData = badgepages;
       }
 
-      await this.set("badgepages", savedData, profile.id);
+      await this.set("badgepages", savedData, profileid);
    },
    getAppDatas: async function(appids) {
       return await this.get("app_data", appids);
    },
-   setAppData: async function(appid) {
-      let savedData = await this.get("app_data", appid);
-      if(savedData[appid]) {
-         savedData = savedData[appid];
-         let data = Profile.appMetaData[appid];
-         savedData.appid = data.appid || savedData.appid;
-         savedData.name  = data.name || savedData.name;
-         savedData.cards = data.cards || savedData.cards;
-         for(let [rarity, badgeList] of Object.entries(data.badges)) {
+   setAppData: async function(appdata) {
+      let savedData = await this.get("app_data", appdata.appid);
+      if(savedData[appdata.appid]) {
+         savedData = savedData[appdata.appid];
+         savedData.appid ??= appdata.appid;
+         savedData.name  ??= appdata.name;
+         savedData.cards ??= appdata.cards;
+         for(let [rarity, badgeList] of Object.entries(appdata.badges)) {
             for(let [level, imgLink] of Object.entries(badgeList)) {
                savedData.badges[rarity][level] = imgLink;
             }
          }
       } else {
-         savedData = Profile.appMetaData[appid];
+         savedData = appdata;
       }
 
-      await this.set("app_data", savedData, appid);
+      await this.set("app_data", savedData, savedData.appid);
    },
    getItemDescripts: async function(appid, contextid, classids) {
       let getList = classids.map(x => `${appid}_${contextid}_${x}`);
       return await this.get("item_descripts", getList);
    },
-   setItemDescripts: function(item, contextid, appid) {
+   setItemDescripts: async function(item, contextid, appid) {
       let key = `${item.appid || appid}_${item.contextid || contextid}_${item.classid}`;
       let savedData = await this.get("item_descripts", key);
       if(savedData[key]) {
@@ -193,9 +218,9 @@ const SteamToolsDbManager = {
       let getList = contextids.map(x => `${profileid}_${appid}_${x}`);
       return await this.get("inventories", getList);
    },
-   setProfileInventory: async function(profile, contextid, appid) {
+   setProfileInventory: async function(inventoryData, profileid, appid, contextid) {
       // No need to update sublevel data, overwrite all old data
-      await this.set("inventories", profile.inventory, `${profile.id}_${appid}_${contextid}`);
+      await this.set("inventories", inventoryData, `${profileid}_${appid}_${contextid}`);
    },
    getMatchResults: async function(profileid1, profileid2List) {
       let getList = profileid2List.map(x => `${profileid1}_${x}`);
