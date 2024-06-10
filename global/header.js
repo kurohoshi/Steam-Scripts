af6b3bc2f6f1ddd2010527c358ce6eb772930080
郑淩星 <xingling39@gmail.com>
2024-06-09 23:25:12 -0500

    Update combined userscript and version date

--- global/header.js
+++ global/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Tools (PLACEHOLDER)
 // @namespace    https://steamcommunity.com/id/KuroHoshiZ/
-// @version      2024-06-06
+// @version      2024-06-09
 // @description  Set of tools to help with Steam Community activities
 // @author       KurohoshiZ
 // @match        *://steamcommunity.com/*
