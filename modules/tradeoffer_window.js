9049caaa0b93d436e7e0dde5b6a695f23194dfc9
郑淩星2 <xingling39@gmail.com>
2024-09-12 14:09:13 -0500

    Add individual setup functions for each feature

--- modules/tradeoffer_window.js
+++ modules/tradeoffer_window.js
@@ -2,8 +2,22 @@ const TradeofferWindow = {
     SETTINGSDEFAULTS: {
     },
 
+    FEATURE_LIST: [
+        { name: prefilter, title: '' },
+        { name: quickSearch, title: '' },
+        { name: itemsSelector, title: '' },
+        { name: message, title: '' },
+        { name: summary, title: '' },
+    ],
+
     shortcuts: {},
 
     setup: function() {
     },
+
+    prefilterSetup: function() { },
+    quickSearchSetup: function() { },
+    itemsSelectorSetup: function() { },
+    messageSetup: function() { },
+    summarySetup: function() { },
 };
