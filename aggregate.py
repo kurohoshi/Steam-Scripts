c0600978f1fc52f8862773c4a1bc6073cc4e96a2
郑淩星2 <xingling39@gmail.com>
2024-09-14 17:11:11 -0500

    Add Camel Case formatting
     - Small formatting changes

--- aggregate.py
+++ aggregate.py
@@ -3,9 +3,18 @@ import os
 import re
 import json
 
+
+def toCamelCase(str):
+    camelStr = re.sub(r'(\s|\u180B|\u200B|\u200C|\u200D|\u2060|\uFEFF|\-|\_)+', ' ', str)
+    camelStr = ''.join( (c.upper() if camelStr[i-1] == ' ' else c) for i, c in enumerate(camelStr) )
+    camelStr = camelStr.replace(' ', '')
+    camelStr = camelStr[0].lower() + camelStr[1:] if len(camelStr) != 0 else camelStr
+    return camelStr
+
+
 o_file_name = 'script.user.js'
 if len(sys.argv) > 1:
-    if sys.argv[1]=='test':
+    if sys.argv[1] == 'test':
         o_file_name = 'testscript.js'
 
 o_file = open(o_file_name, 'w', encoding="utf-8")
@@ -16,12 +25,12 @@ with open('global/config.json', 'r', encoding="utf-8") as config_file:
 print(config)
 global_dir_entries = os.listdir('global')
 
-global_dir_entries = list(filter(lambda x: x.endswith('.js'), global_dir_entries))
+global_dir_entries = list( filter(lambda x: x.endswith('.js'), global_dir_entries) )
 global_dir_entries = [x.split('.')[0] for x in global_dir_entries if x.split('.')[0] in config]
 global_dir_entries.sort(key=lambda x: config[x.split('.')[0]])
 
 # Append global functions
-for dir_entry in filter(lambda x: config[x]<=0, global_dir_entries):
+for dir_entry in filter(lambda x: config[x] <= 0, global_dir_entries):
     print(dir_entry+'.js')
     with open(f'global/{dir_entry}.js', 'r', encoding="utf-8") as js_module:
         for line in js_module:
@@ -39,7 +48,7 @@ for dir_entry in os.scandir('modules'):
         o_file.write('\n')
 
 # Append the rest of global functions
-for dir_entry in filter(lambda x: config[x]>0, global_dir_entries):
+for dir_entry in filter(lambda x: config[x] > 0, global_dir_entries):
     print(dir_entry+'.js')
     with open(f'global/{dir_entry}.js', 'r', encoding="utf-8") as js_module:
         for line in js_module:
@@ -51,7 +60,7 @@ for dir_entry in os.scandir('css'):
     print(dir_entry.name)
 
     with open(dir_entry.path, 'r', encoding="utf-8") as css_file:
-        o_file.write(f'\n const css{dir_entry.name[:-4].capitalize()} = `')
+        o_file.write(f'\n const {toCamelCase('css ' + dir_entry.name[:-4])} = `')
         for line in css_file:
             o_file.write(line)
         o_file.write('`;')
