ac124250f7ce8601c2cbfe91fb4cfb25b99f05d6
郑淩星 <xingling39@gmail.com>
2024-10-28 02:00:31 -0500

    Modify badgepage card visuals based on quantity
     - Change button list detection to not throw if it doesn't exist in
       order to continue to card visual modifications
     - Change card visuals if quantity is not shown, or is 0

--- modules/badgepage_extras.js
+++ modules/badgepage_extras.js
@@ -6,31 +6,60 @@ const BadgepageExtras = {
             throw 'BadgepageForumButton.setup(): appid not found?';
         }
 
-        // Add forum button link
+
+
         let badgepageButtonsElem = document.querySelector('.gamecards_inventorylink');
         if(!badgepageButtonsElem) {
-            throw 'BadgepageForumButton.setup(): buttons list not found?';
+            console.warn('BadgepageForumButton.setup(): buttons list not found?');
+        } else {
+            let htmlStringList = [];
+
+            // Add forum button link
+            let forumButtonHTMLString = `<a target="_blank" class="btn_grey_grey btn_medium" href="https://steamcommunity.com/app/${appid}/tradingforum">`
+              +     '<span>Visit Trade Forum</span>'
+              + '</a>';
+            htmlStringList.push(forumButtonHTMLString);
+
+            // Add foil/normal badgepage button link
+            let isFoilPage = window.location.search.includes('border=1');
+            let badgepageUrlString = badgepageUrl;
+            if(!isFoilPage) {
+                badgepageUrlString += '?border=1';
+            }
+            let foilToggleButtonHTMLString = `<a class="btn_grey_grey btn_medium" href="${badgepageUrlString}">`
+              +     `<span>${isFoilPage ? 'Normal' : 'Foil'} Badge Page</span>`
+              + '</a>';
+            htmlStringList.push(foilToggleButtonHTMLString);
+
+            badgepageButtonsElem.insertAdjacentHTML('afterbegin', htmlStringList.join(' '));
         }
 
-        let htmlStringList = [];
 
-        let forumButtonHTMLString = `<a target="_blank" class="btn_grey_grey btn_medium" href="https://steamcommunity.com/app/${appid}/tradingforum">`
-          +     '<span>Visit Trade Forum</span>'
-          + '</a>';
-        htmlStringList.push(forumButtonHTMLString);
 
-        // Add foil/normal badgepage button link
-        let isFoilPage = window.location.search.includes('border=1');
-        let badgepageUrlString = badgepageUrl;
-        if(!isFoilPage) {
-            badgepageUrlString += '?border=1';
+        // Set unowned cards to the proper overlay/border
+        // WARNING: Currently causes quick flash of owned cards before getting changed to unowned
+        let cardElemSetUnowned = (elem) => {
+            elem.classList.remove('owned');
+            elem.classList.add('unowned');
+            if(!elem.querySelector('.game_card_unowned_border')) {
+                elem.firstElementChild.insertAdjacentHTML('afterbegin', '<div class="game_card_unowned_border"></div>');
+            }
+        };
+
+        for(let cardElem of document.querySelectorAll('.badge_card_set_card')) {
+            let cardQtyElem = cardElem.querySelector('.badge_card_set_text_qty');
+            if(!cardQtyElem) {
+                if(cardElem.classList.contains('owned')) {
+                    cardElemSetUnowned(cardElem);
+                }
+                continue;
+            }
+
+            let cardQty = parseInt(cardQtyElem.textContent.replace(/^\(|\)$/g, ''));
+            if((!Number.isInteger(cardQty) || cardQty === 0) && cardElem.classList.contains('owned')) {
+                cardElemSetUnowned(cardElem);
+            }
         }
-        let foilToggleButtonHTMLString = `<a class="btn_grey_grey btn_medium" href="${badgepageUrlString}">`
-          +     `<span>${isFoilPage ? 'Normal' : 'Foil'} Badge Page</span>`
-          + '</a>';
-        htmlStringList.push(foilToggleButtonHTMLString);
-
-        badgepageButtonsElem.insertAdjacentHTML('afterbegin', htmlStringList.join(' '));
 
         // Optional: delete other trade forum buttons in the friends with cards section
         // WARNING: May or may not break other modules that might use these buttons
